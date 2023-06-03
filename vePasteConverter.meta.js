b3a584d26e6cb55b002bf691a0bd3f626461d467
Nux <eccenux+git@gmail.com>
2023-06-03 18:10:38 +0200

    gulp build

---
+++ vePasteConverter.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @author      Eccenux
+// @name        wiki vePasteConverter Cytuj
+// @namespace   https://github.com/jonatkins/ingress-intel-total-conversion
+// @version     0.0.1
+// @description [0.0.1] Konwerter szablonów cytowania wklejonych z VE; Cytuj (refs).
+// @match       http://tools.wikimedia.pl/~malarz_pl/cgi-bin/convert.pl*
+// @match       https://tools.wikimedia.pl/~malarz_pl/cgi-bin/convert.pl*
+// @grant       none
+// ==/UserScript==
+
---
+++ vePasteConverter.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @author      Eccenux
+// @name        wiki vePasteConverter Cytuj
+// @namespace   https://github.com/jonatkins/ingress-intel-total-conversion
+// @version     0.0.1
+// @description [0.0.1] Konwerter szablonów cytowania wklejonych z VE; Cytuj (refs).
+// @updateURL   https://github.com/Eccenux/wiki-vePasteConverter-Cytuj/raw/master/vePasteConverter.meta.js
+// @downloadURL https://github.com/Eccenux/wiki-vePasteConverter-Cytuj/raw/master/vePasteConverter.user.js
+// @match       http://tools.wikimedia.pl/~malarz_pl/cgi-bin/convert.pl*
+// @match       https://tools.wikimedia.pl/~malarz_pl/cgi-bin/convert.pl*
+// @grant       none
+// ==/UserScript==
+
+var _css = /*css*/ `
+
+`;
+
+// init plugin namespace for exposed stuff
+if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+// _css will be provided via `gulp` build.
+window.plugin.vePasteConverter = {
+	CSS: _css,
+	setup: ()=>{},
+};
+
+/**
+ * VE Paste core.
+ */
+var VePasta = class {
+}
+// PLUGIN END //////////////////////////////////////////////////////////
+
+window.plugin.vePasteConverter.setup();
